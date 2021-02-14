2e9708946918909a893f6c0e5892984df2576585
pmmson <pmmson@yandex.ru>
2021-02-14 13:00:52 +0300

    Solution HW01

---
+++ hw01/task02.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/*
+2. Объявить две переменные: admin и name. Записать в name строку "Василий". Скопировать значение из name в admin. Вывести в консоль переменную admin (должно вывести "Василий").
+*/
+let admin = null;
+let name = "Василий";
+admin = name;
+console.log(admin);
